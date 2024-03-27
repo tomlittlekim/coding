9b4371f1cc565ca9686cf10b9aa1a6f8349b435b
tomlittlekim <youngmini90@naver.com>
2024-03-27 15:40:43 +0900

    Changes

--- test/MainTest.java
+++ test/MainTest.java
@@ -1,41 +1,43 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class MainTest {
 
+    Main main = new Main();
+
+    @ParameterizedTest
+    @CsvSource({"98,2,1", "34,3,0"})
+    void exam_5(int num, int n, int expected) {
+        Assertions.assertEquals(expected, main.exam_5(num, n));
+    }
+
     @Test
     void exam_1() {
-        Main main = new Main();
         Assertions.assertEquals("abc", main.exam_1(new String[]{"a","b","c"}));
     }
 
-    @Test
-    void exam_2() {
-        Main main = new Main();
-        String my_string_1 = "string";
-        String my_string_2 = "love";
-
-        String my_string_1_answer = "stringstringstring";
-        String my_string_2_answer = "lovelovelovelovelovelovelovelovelovelove";
-
-        Assertions.assertEquals(my_string_1_answer, main.exam_2(my_string_1, 3));
-        Assertions.assertEquals(my_string_2_answer, main.exam_2(my_string_2, 10));
+    @ParameterizedTest
+    @CsvSource({
+        "string, 3, stringstringstring"
+        , "love, 10, lovelovelovelovelovelovelovelovelovelove"
+    })
+    void exam_2(String str, int n, String answer) {
+        Assertions.assertEquals(answer, main.exam_2(str, n));
     }
 
-    @Test
-    void exam_3() {
-        Main main = new Main();
-
-        Assertions.assertEquals(991, main.exam_3(9, 91));
-        Assertions.assertEquals(898, main.exam_3(89, 8));
+    @ParameterizedTest
+    @CsvSource({"991,9,91", "898,89,8"})
+    void exam_3(int expected, int a, int b) {
+        Assertions.assertEquals(expected, main.exam_3(a, b));
     }
 
-    @Test
-    void exam_4() {
-        Main main = new Main();
-
-        Assertions.assertEquals(364, main.exam_4(2, 91));
-        Assertions.assertEquals(912, main.exam_4(91, 2));
+    @ParameterizedTest
+    @CsvSource({"364,2,91", "912,91,2"})
+    void exam_4(int expected, int a, int b) {
+        Assertions.assertEquals(expected, main.exam_4(a, b));
     }
 
 }
